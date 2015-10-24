c139a5dc717c8a407ee8aa354207c624af52ae5a
LeiWang <wangleiever@outlook.com>
2015-10-24 20:06:41 +0800

    1.创建工程，导入需要的库

---
+++ PaperWhite/PaperWhite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaperWhite
+//
+//  Created by qianfeng001 on 15/10/24.
+//  Copyright (c) 2015年 王磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
